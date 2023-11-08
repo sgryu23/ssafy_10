25e890e0d75100d2b2f06d1f5d90bd7a5b215f2d
sgr <sg.ryu23@gmail.com>
2023-11-08 17:56:59 +0900

    TIL 231108 upload

---
+++ 231108_Component_State_Flow/vue_hw_5_2_completed code/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <ParentPage />
+  </div>
+</template>
+
+<script setup>
+import ParentPage from '@/components/ParentPage.vue'
+</script>
+
+<style scoped>
+  h1 {
+    font-weight: 600;
+  }
+  h3 {
+    font-weight: 300;
+  }
+</style>
---
+++ 231108_Component_State_Flow/vue_hw_5_2_completed code/src/components/ParentPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>부모 페이지입니다.</h1>
+    <ChildPage :my-child="children" />
+  </div>
+</template>
+
+<script setup>
+import ChildPage from '@/components/ChildPage.vue'
+import { ref } from 'vue'
+
+const children = ref([
+    { name: '김하나', age: 30 },
+    { name: '김두리', age: 20 },
+    { name: '김서이', age: 10 }
+  ])
+
+</script>
+
+<style scoped>
+
+</style>
